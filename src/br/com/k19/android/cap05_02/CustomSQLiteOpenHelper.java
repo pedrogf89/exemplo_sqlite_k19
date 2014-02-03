6e9f743330fcca29a080d9d17463bf0eea1b57d6
pedrofernandes <pedrofernandes@guarulhos.sp.gov.br>
2014-02-03 16:56:47 -0200

    Alteracao dos arquivos para o NotesDao

--- src/br/com/k19/android/cap05_02/CustomSQLiteOpenHelper.java
+++ src/br/com/k19/android/cap05_02/CustomSQLiteOpenHelper.java
@@ -8,7 +8,7 @@ public class CustomSQLiteOpenHelper extends SQLiteOpenHelper {
 
 	public static final String TABLE_NOTES = "notes";
 	public static final String COLUMN_ID = "_id";
-	public static final String COUMN_NOTES = "note";
+	public static final String COLUMN_NOTES = "note";
 
 	private static final String DATABASE_NAME = "notes.db";
 	private static final int DATABASE_VERSION = 1;
@@ -17,7 +17,7 @@ public class CustomSQLiteOpenHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_CREATE = "create table " + TABLE_NOTES
 			+ "(" + COLUMN_ID + " integer primary key autoincrement, "
-			+ COUMN_NOTES + " text not null);";
+			+ COLUMN_NOTES + " text not null);";
 
 	public CustomSQLiteOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
--- src/br/com/k19/android/cap05_02/NotesDao.java
+++ src/br/com/k19/android/cap05_02/NotesDao.java
@@ -1,11 +1,23 @@
 package br.com.k19.android.cap05_02;
 
+import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class NotesDao {
 
 	private SQLiteDatabase database;
-	//pagina 43 da apostila
+	private String[] columns = { CustomSQLiteOpenHelper.COLUMN_ID,
+			CustomSQLiteOpenHelper.COLUMN_NOTES };
+	private CustomSQLiteOpenHelper sqliteOpenHelper;
 	
-	//teste de inclusão no github
+	public NotesDao(Context context){
+		sqliteOpenHelper = new CustomSQLiteOpenHelper(context);
+	}
+	public void open() throws SQLException{
+		database = sqliteOpenHelper.getWritableDatabase();
+	}
+	public void close(){
+		sqliteOpenHelper.close();
+	}
 }
